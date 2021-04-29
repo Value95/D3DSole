bb0b187625c8e2ec0ba9d66775fb50d16b8679e8
value95 <ehddlf1223@naver.com>
2021-04-29 19:36:42 +0900

    버림

---
+++ Engine/Code/EffectComponent.cpp
@@ -0,0 +1,193 @@
+#include "EngineStdafx.h"
+#include "EffectComponent.h"
+#include "DeviceManager.h"
+#include "GameObject.h"
+#include "TextureStore.h"
+#include "AnimMeshRenderManager.h"
+
+#include "SceneManager.h"
+
+USING(Engine)
+CEffectComponent::CEffectComponent()  
+{
+}
+
+
+CEffectComponent::~CEffectComponent(void)
+{
+}
+
+SHARED(CComponent) CEffectComponent::MakeClone(CGameObject* pObject)
+{
+	SHARED(CEffectComponent) pClone(new CEffectComponent);
+	pClone->SetOwner(pObject);
+	pClone->SetName(m_name);
+
+	pClone->SetIsAwaked(m_isAwaked);
+
+	pClone->SetEffectCount(m_effectCount);
+	pClone->SetTexData(m_texData);
+
+	return pClone;
+}
+
+void CEffectComponent::Awake(void)
+{
+	__super::Awake();
+}
+
+
+void CEffectComponent::Start(SHARED(CComponent) spThis)
+{
+	__super::Start(spThis);
+
+	std::wstring layerKey = m_pOwner->GetLayerKey();
+	std::wstring objectKey = m_pOwner->GetObjectKey();
+
+	m_effectCurCount = 0;
+
+	for (int i = 0; i < m_effectCount; i++)
+	{
+		std::wstring textureKey = m_textureKey + to_wstring(i);
+		m_texData.emplace_back(CTextureStore::GetInstance()->GetTextureData(textureKey));
+		if (m_texData[i] == nullptr)
+			m_texData[i] = CTextureStore::GetInstance()->GetTextureData(L"Error");
+	}
+
+	DateInit();
+}
+
+_uint CEffectComponent::FixedUpdate(SHARED(CComponent) spThis)
+{
+	return NO_EVENT;
+}
+
+_uint CEffectComponent::Update(SHARED(CComponent) spThis)
+{
+	if (m_effectSpeed <= 0)
+	{
+		m_effectCurCount++;
+		if (m_effectCurCount == m_effectCount)
+		{
+			m_effectCurCount = 0;
+		}
+	}
+
+	m_effectSpeed -= deltaTime;
+	return NO_EVENT;
+}
+
+_uint CEffectComponent::LateUpdate(SHARED(CComponent) spThis)
+{
+	CAnimMeshRenderManager::GetInstance()->AddToEffectRenderList(std::dynamic_pointer_cast<CEffectComponent>(spThis));
+
+	return NO_EVENT;
+}
+
+_uint CEffectComponent::PreRender(void)
+{
+	// 데이터 셋팅
+	if (GetOwner() == nullptr)
+		MSG_BOX(__FILE__, L"Owner is nullptr");
+
+
+	GET_DEVICE->SetStreamSource(0, m_meshDate.vertexBuffer, 0, m_meshDate.vertexSize);
+	GET_DEVICE->SetFVF(m_meshDate.FVF);
+	GET_DEVICE->SetIndices(m_meshDate.indexBuffer);
+
+	if (m_texData[m_effectCurCount] != nullptr)
+		GET_DEVICE->SetTexture(0, m_texData[m_effectCurCount]->pTexture);
+	else
+		GET_DEVICE->SetTexture(0, nullptr);
+
+	//렌더 셋팅
+
+	//좌표셋팅
+	GET_DEVICE->SetTransform(D3DTS_WORLD, &GetOwner()->GetWorldMatrix());
+	GET_DEVICE->SetTransform(D3DTS_VIEW, &GET_CUR_SCENE->GetMainCamera()->GetViewMatrix());
+	GET_DEVICE->SetTransform(D3DTS_PROJECTION, &GET_CUR_SCENE->GetMainCamera()->GetProjMatrix());
+
+
+	return _uint();
+}
+
+_uint CEffectComponent::Render(void)
+{
+	GET_DEVICE->DrawIndexedPrimitive(D3DPT_TRIANGLELIST, 0, 0, m_meshDate.vertexCount, 0, m_meshDate.faceCount);
+
+	GET_DEVICE->SetTransform(D3DTS_PROJECTION, &GET_CUR_SCENE->GetMainCamera()->GetProjMatrix());
+	return _uint();
+}
+
+_uint CEffectComponent::PostRender(void)
+{
+	return _uint();
+}
+
+void CEffectComponent::OnDestroy(void)
+{
+}
+
+void CEffectComponent::OnEnable(void)
+{
+}
+
+void CEffectComponent::OnDisable(void)
+{
+}
+
+void CEffectComponent::Reset(std::wstring textureKey, _int effectCount)
+{
+	m_textureKey = textureKey;
+	m_effectCount = effectCount;
+}
+
+void CEffectComponent::DateInit()
+{
+	m_meshDate.FVF = customFVF;
+	m_meshDate.vertexSize = sizeof(_CustomVertex);
+	m_meshDate.vertexNumInFace = 3;
+	// FVF를 지정하여 보관할 데이터의 형식을 지정하고 사용자 정점을 보관할 메모리할당
+	GET_DEVICE->CreateVertexBuffer(4 * sizeof(_CustomVertex), 0, m_meshDate.FVF, D3DPOOL_MANAGED, &m_meshDate.vertexBuffer, NULL);
+
+	_CustomVertex* pVertices = nullptr;
+
+	m_meshDate.vertexBuffer->Lock(0, 0, (void**)&pVertices, 0);
+	m_meshDate.vertexCount = 4;
+
+	pVertices[0].position = vector3(-0.5f, 0.5f, 0);
+	pVertices[0].uv = vector2(0, 0);
+
+	pVertices[1].position = vector3(0.5f, 0.5f, 0);
+	pVertices[1].uv = vector2(1, 0);
+
+	pVertices[2].position = vector3(0.5f, -0.5f, 0);
+	pVertices[2].uv = vector2(1, 1);
+
+	pVertices[3].position = vector3(-0.5f, -0.5f, 0);
+	pVertices[3].uv = vector2(0, 1);
+		
+	pVertices[0].normal = vector3Back;
+	pVertices[1].normal = vector3Back;
+	pVertices[2].normal = vector3Back;
+	pVertices[3].normal = vector3Back;
+
+	m_meshDate.vertexBuffer->Unlock();
+
+	GET_DEVICE->CreateIndexBuffer(6 * sizeof(WORD), 0, D3DFMT_INDEX16, D3DPOOL_DEFAULT, &m_meshDate.indexBuffer, NULL);
+	m_meshDate.indexSize = sizeof(WORD);
+	WORD* pIndices = nullptr;
+
+	m_meshDate.indexBuffer->Lock(0, 0, (void**)&pIndices, 0);
+	m_meshDate.faceCount = 2;
+
+	pIndices[0] = 0;
+	pIndices[1] = 1;
+	pIndices[2] = 2;
+	pIndices[3] = 0;
+	pIndices[4] = 2;
+	pIndices[5] = 3;
+
+	m_meshDate.indexBuffer->Unlock();
+
+}
---
+++ Reference/Header/EffectComponent.h
@@ -0,0 +1,45 @@
+#ifndef EFFECTCOMPONENT_H
+#define EFFECTCOMPONENT_H
+
+BEGIN(Engine)
+class ENGINE_DLL CEffectComponent final : public CComponent
+{
+private:
+	std::wstring m_textureKey; // 이미지를 불러올 키값
+	GETTOR_SETTOR(_int, m_effectCount, 0, EffectCount)
+	GETTOR_SETTOR(std::vector<SHARED(_TexData)>, m_texData, {}, TexData) // 이미지를 저장할 변수
+
+
+	_MeshData m_meshDate; // 버텍스 ,인덱스 버퍼를저장하기 위한 변수
+	_int m_effectCurCount;
+	_int m_effectSpeed;
+public:
+	explicit						CEffectComponent	(void);
+	virtual						   ~CEffectComponent	(void);
+
+public:
+	SHARED(CComponent)	MakeClone			(CGameObject* pObject) override;
+
+	void Awake(void) override;
+	void Start(SHARED(CComponent) spSelf) override;
+
+	_uint FixedUpdate(SHARED(CComponent) spSelf) override;
+	_uint Update(SHARED(CComponent) spThis) override;
+	_uint LateUpdate(SHARED(CComponent) spThis) override;
+		  
+	_uint PreRender(void);
+	_uint Render(void);
+	_uint PostRender(void);
+
+	void OnDestroy(void) override;
+		 
+	void OnEnable(void) override;
+	void OnDisable(void) override;
+
+	void Reset(std::wstring textureKey, _int effectCount);
+private:
+	void DateInit();
+};
+END
+
+#endif // !TEXTURECOMPONENT_H
